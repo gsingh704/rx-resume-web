6a61a28590b9549afa0be78e49c17703dd536971
Gurjant Singh <85999340+gsingh704@users.noreply.github.com>
2025-05-23 20:49:52 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
  
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base : '/rx-resume-web/',
   server: {
     watch: {
       usePolling: true, 
