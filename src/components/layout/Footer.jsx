2a34b77d002e994b1483cb9f0cef77739068de7c
Gurjant Singh <85999340+gsingh704@users.noreply.github.com>
2025-05-23 21:22:59 +0200

    fixed footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = ({ showAttribution }) => {
   return (
     <div className="text-center text-gray-500 dark:text-gray-400 mt-10 py-6 border-t dark:border-gray-700">
       <p className="mb-1">
-        Generated with
+        Generated with S
         <a
-          href="https://www.github.com/gsingh704/Reactive-Resume-Web"
+          href="https://github.com/gsingh704/rx-resume-web"
           target="_blank"
           rel="noopener noreferrer"
-          className="hover:underline text-indigo-500 dark:text-indigo-400"
+          className="hover:underline text-indigo-500 dark:text-indigo-400 ml-2"
         >
-          Reactive Resume Web Generator
+           Reactive Resume Web Generator
         </a>
       </p>
     </div>
